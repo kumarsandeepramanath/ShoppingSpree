e3442600432b3a48f63ec05b95c97e2cd9ed7bbd
Kumar Sandeep Ramanath <kumarsandeepr89@gmail.com>
2021-04-12 22:57:46 +0530

    Boiler for React 3

--- shopping/src/App.js
+++ shopping/src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload This file will be
-          deleted.
+          deleted. Boiler plate code to be removed
         </p>
         <a
           className="App-link"
